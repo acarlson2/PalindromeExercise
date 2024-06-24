9fe5c528790d7c349e3adf47cb6bb5146822a8b0
Aaron Carlson <acarlson2@carthage.edu>
2024-06-24 13:00:35 -0500

    exercise complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public WordSmith()
+		{
+		}
+
+		public bool IsAPalindrome(string input)
+		{
+			char[] asArray = input.ToCharArray();
+			Array.Reverse(asArray);
+			string reversed = new string(asArray);
+
+			if(input == reversed)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("tacocat", true)]
+        [InlineData("goatmeat", false)]
+        [InlineData("hannah", true)]
+        [InlineData("dogdog", false)]
+        public void Test1(string str, bool expected)
         {
+            WordSmith wordSmith = new WordSmith();
 
+            bool actual = wordSmith.IsAPalindrome(str);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
